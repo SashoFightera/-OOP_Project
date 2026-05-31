a6efb1d14d3df5f75878aa3a89b9190f71909645
SashoFightera <alex0110.milanov@gmail.com>
2026-05-31 12:56:59 +0300

    feat: validate tag name is not empty in Tag constructor

--- src/Tag.cpp
+++ src/Tag.cpp
@@ -1,7 +1,11 @@
 #include "../include/Tag.h"
+#include <stdexcept>
 
 Tag::Tag(const std::string& name, const std::string& color)
-    : name(name), color(color) {}
+    : name(name), color(color) {
+        if (name.empty())
+        throw std::invalid_argument("Tag name cannot be empty.");
+    }
 
 std::string Tag::getName()  const { return name; }
 std::string Tag::getColor() const { return color; }
