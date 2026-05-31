4c1931c61ee16c0b630572f3c8297a1e9dccc171
SashoFightera <alex0110.milanov@gmail.com>
2026-05-31 12:54:06 +0300

    feat: add basic email validation in User constructor

--- src/User.cpp
+++ src/User.cpp
@@ -1,7 +1,11 @@
 #include "../include/User.h"
+#include <stdexcept>
 
 User::User(int id, const std::string& username, const std::string& email)
-    : Entity(id), username(username), email(email) {}
+    : Entity(id), username(username), email(email) {
+    if (email.find('@') == std::string::npos)
+        throw std::invalid_argument("Invalid email: missing '@'");
+}
 
 std::string User::getUsername() const { return username; }
 std::string User::getEmail()    const { return email; }
